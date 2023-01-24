4345353f56409b04454626d3c5707816fa946409
flaviomartil <flaviomartil5@gmail.com>
2023-01-24 06:42:59 -0300

    Comitando script painel

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+---- Evolution Software Inc
+
+fx_version "adamant"
+game "gta5"
+
+ui_page "nui/index.html"
+
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"config.lua",
+	"client.lua",
+} 
+
+server_script {
+	"@vrp/lib/utils.lua",
+	"config.lua",
+	"server.lua"
+}
+
+files {
+	"nui/index.html",
+	"nui/script.js",
+	"nui/css.css",
+	"painel/*.png",
+    "painel/styles.css",
+    "painel/index.html",
+    "painel/functions.js",
+    "painel/jEscape.min.js",
+}
+-- [[!-!]] s5CPmoXcxszMzoPKx83Lz8fHy87MzcrKyMnNz80= [[!-!]] --              
